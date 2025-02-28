aaa2feac8e741ac6d75f44ece2e22ede708809e8
Artem Chernikov <alatris90@gmail.com>
2025-03-01 00:23:04 +0200

    1.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {CounterProvider} from "./context/CounterContext";
-import App from './App'
+import {CounterProvider} from "./context/CounterContext.jsx";
+import App from './App.jsx'
 import './styles/main.scss'
 import store from "./Store/store.jsx";
 import {Provider} from "react-redux";
